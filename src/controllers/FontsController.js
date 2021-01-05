303da14cce79df8d31e8f86398d7dea0ca2083ee
brunoannunciato <brunobannunciato@gmail.com>
2021-01-05 12:00:37 -0300

    feat: add fonts de 32px e 64px

--- src/controllers/FontsController.js
+++ src/controllers/FontsController.js
@@ -1,5 +1,7 @@
 const FontsController = {
-  comicSams16Yellow: './src/fonts/comic-sans-16-yellow/comic-sans-16-yellow.fnt'
+  comicSams16Yellow: './src/fonts/comic-sans-16-yellow/comic-sans-16-yellow.fnt',
+  comicSams32Yellow: './src/fonts/comic-sans-32-yellow/comic-sans-32-yellow.fnt',
+  comicSams64Yellow: './src/fonts/comic-sans-64-yellow/comic-sans-64-yellow.fnt'
 }
 
 module.exports = FontsController
