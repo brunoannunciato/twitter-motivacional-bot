69e57eb2e60379db5afc2e731378698b99e05027
brunoannunciato <brunobannunciato@gmail.com>
2021-01-09 16:11:23 -0300

    refactor: remoção de objeto de imagem

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const Twit = require('twit');
 const config = require('./config')
 const ImageController = require('./src/controllers/ImagesController')
 
-const image = {
-  path: './src/rawImages/image-3.png'
-}
-
 const text = {
   content: 'Teu cuzin!',
   author: 'bu_nro'
 }
 
-ImageController.generate(image, text)
+ImageController.generate(text)
