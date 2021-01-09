e5f856a8290ef36e09c8e885541615398eee7578
brunoannunciato <brunobannunciato@gmail.com>
2021-01-09 13:50:29 -0300

    feat: Mock do que o twitter deverá retornar

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 require('dotenv').config()
 const Twit = require('twit');
-const config = require('./config')
 
-const T = new Twit(config)
+const config = require('./config')
+const ImageController = require('./src/controllers/ImagesController')
+
+const image = {
+  path: './src/rawImages/image-3.png'
+}
+
+const text = {
+  content: 'Teu cuzin!',
+  author: 'bu_nro'
+}
+
+ImageController.generate(image, text)
