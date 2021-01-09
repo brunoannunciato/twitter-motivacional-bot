5b4d504b31cb79bda0158b8886a337454caad25c
brunoannunciato <brunobannunciato@gmail.com>
2021-01-09 13:49:52 -0300

    feat: add nome do autor em imagem

--- src/controllers/ImagesController.js
+++ src/controllers/ImagesController.js
@@ -3,13 +3,41 @@ const Jimp = require('jimp')
 const Fonts = require('./FontsController')
 
 const ImageController = {
+  getAuthorPosition: function(font, text) {
+    const textHeight = Jimp.measureTextHeight(font, text.content, 1024)
+
+    return textHeight
+  },
   generate: async function(imageConfig, text) {
     const image = await Jimp.read(imageConfig.path)
-    const font = await Jimp.loadFont(Fonts.comicSams16Yellow);
+    const textFont = await Jimp.loadFont(Fonts.comicSams64Yellow);
+    const creditFont = await Jimp.loadFont(Fonts.comicSams16Yellow);
 
-    image.print(font, 10, 10, 'Teste 2!')
+    image.print(
+      textFont,
+      0,
+      200,
+      {
+        text: `"${ text.content }"`,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_TOP
+      },
+      1024,
+      400
+    )
+    .print(
+      creditFont,
+      0,
+      200 + this.getAuthorPosition(textFont, text),
+      {
+        text: `- @${text.author}`,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_TOP
+      },
+      1024,
+      400
+    )
     .write('./output/image.png')
-    //console.log(image)
   }
 }
 
